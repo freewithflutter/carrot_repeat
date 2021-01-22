61552c865a730b2247baeea7cd5bad59b5055005
Joo-esc <hj_lee_com@naver.com>
2021-01-22 11:24:46 +0900

    stream,detail,popupitem where repeat (2)

--- lib/examdetail.dart
+++ lib/examdetail.dart
@@ -4,24 +4,48 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class ExamDetail extends StatelessWidget {
-  static final String id = 'examDetail';
+  static String id = 'examDetail';
+  final fireStore = FirebaseFirestore.instance;
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<ItemProvider>(context, listen: false);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('detail exam'),
-      ),
-      body: StreamBuilder(
-          stream: FirebaseFirestore.instance
-              .collection('Items')
-              .doc(provider.selectedId)
-              .snapshots(),
-          builder: (context, snapshot) {
-            return Container(
-              child: Image.network(snapshot.data.data()['image']),
-            );
-          }),
-    );
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('Items')
+            .doc(provider.selectedId)
+            .snapshots(),
+        builder: (context, snapshot) {
+          return Column(
+            children: [Image.network(snapshot.data.data()['image'])],
+          );
+        });
   }
 }
+
+// import 'package:carrot_repeat/provider/item_provider.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+//
+// class ExamDetail extends StatelessWidget {
+//   static final String id = 'examDetail';
+//   @override
+//   Widget build(BuildContext context) {
+//     final provider = Provider.of<ItemProvider>(context, listen: false);
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('detail exam'),
+//       ),
+//       body: StreamBuilder(
+//           stream: FirebaseFirestore.instance
+//               .collection('Items')
+//               .doc(provider.selectedId)
+//               .snapshots(),
+//           builder: (context, snapshot) {
+//             return Container(
+//                 // child: Image.network(snapshot.data.data()['image']),
+//                 );
+//           }),
+//     );
+//   }
+// }
